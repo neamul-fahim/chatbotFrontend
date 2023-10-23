da93cdabecde354a42ffea65799df305bf612fc6
neamul-fahim <102896638+neamul-fahim@users.noreply.github.com>
2023-10-23 10:38:37 +0600

    nre

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { CssBaseline, Container, Grid, Paper } from '@mui/material';
+import QuestionList from './components/QuestionList';
+import EditableQuestionList from './components/EditableQuestionList';
+import ChatButton from './components/ChatButton';
+import ChatWindow from './components/ChatWindow';
 
 function App() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      {isChatOpen && <ChatWindow onClose={toggleChat} />}
+      <ChatButton onClick={toggleChat} />
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper elevation={3}>
+              <QuestionList />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper elevation={3}>
+              <EditableQuestionList />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
